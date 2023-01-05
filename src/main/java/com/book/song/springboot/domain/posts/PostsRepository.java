7762b6c2723a64dfe29adbc79411d753cc9c07eb
Song kyeong seok <skj0922@kookmin.ac.kr>
2023-01-05 19:59:18 +0900

    feat(PostsRepository): 게시글 리포지토리 인터페이스 작성

---
+++ src/main/java/com/book/song/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,1 @@
+package com.book.song.springboot.domain.posts;import org.springframework.data.jpa.repository.JpaRepository;public interface PostsRepository extends JpaRepository<Posts, Long> {}

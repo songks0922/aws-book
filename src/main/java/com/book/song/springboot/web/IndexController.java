5e2cfdc58b3b5c51d9cdcbf37239541f6ef0388d
Song kyeong seok <skj0922@kookmin.ac.kr>
2023-01-05 22:17:36 +0900

    feat(index, IndexController): Index view, 컨트롤러 코드 작성

---
+++ src/main/java/com/book/song/springboot/web/IndexController.java
@@ -0,0 +1,1 @@
+package com.book.song.springboot.web;import org.springframework.stereotype.Controller;import org.springframework.web.bind.annotation.GetMapping;@Controllerpublic class IndexController {    @GetMapping("/")    public String index() {        return "index";    }}

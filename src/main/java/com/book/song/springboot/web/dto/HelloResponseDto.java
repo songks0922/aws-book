63b0b72da5c238c20dc94391eefce6c0bd2ea054
Song kyeong seok <skj0922@kookmin.ac.kr>
2023-01-05 19:31:43 +0900

    project init

---
+++ src/main/java/com/book/song/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,1 @@
+package com.book.song.springboot.web.dto;import lombok.Getter;import lombok.RequiredArgsConstructor;@Getter@RequiredArgsConstructorpublic class HelloResponseDto {    private final String name;    private final int amount;}

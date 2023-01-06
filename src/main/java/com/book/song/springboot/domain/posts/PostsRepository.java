33d32689daad1b0a8c237fe9c05874201c45b5a8
Song kyeong seok <skj0922@kookmin.ac.kr>
2023-01-06 12:39:59 +0900

    feat(PostsRepository): 게시글 전체 조회 repository 메서드 작성

--- src/main/java/com/book/song/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/book/song/springboot/domain/posts/PostsRepository.java
@@ -1,1 +1,1 @@
-package com.book.song.springboot.domain.posts;import org.springframework.data.jpa.repository.JpaRepository;public interface PostsRepository extends JpaRepository<Posts, Long> {}
+package com.book.song.springboot.domain.posts;import java.util.List;import org.springframework.data.jpa.repository.JpaRepository;import org.springframework.data.jpa.repository.Query;public interface PostsRepository extends JpaRepository<Posts, Long> {    @Query("select p from Posts p order by p.id desc")    List<Posts> findAllDesc();}

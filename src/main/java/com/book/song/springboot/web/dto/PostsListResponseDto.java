9226ac4fc64bbaf021f62c3421844aee627f1004
Song kyeong seok <skj0922@kookmin.ac.kr>
2023-01-06 13:42:38 +0900

    feat(PostsListResponseDto): 게시글 리스트 dto 클래스 작성

---
+++ src/main/java/com/book/song/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,21 @@
+package com.book.song.springboot.web.dto;
+
+import com.book.song.springboot.domain.posts.Posts;
+import java.time.LocalDateTime;
+import lombok.Getter;
+
+@Getter
+public class PostsListResponseDto {
+
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}

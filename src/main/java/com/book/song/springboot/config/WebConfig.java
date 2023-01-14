f6875390a95ae0a79aa40b5c727186a78480c824
Song kyeong seok <skj0922@kookmin.ac.kr>
2023-01-14 18:10:25 +0900

    feat: 네이버 로그인 구현

---
+++ src/main/java/com/book/song/springboot/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.book.song.springboot.config;
+
+import com.book.song.springboot.config.auth.LoginUserArgumentResolver;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(loginUserArgumentResolver);
+    }
+
+}
